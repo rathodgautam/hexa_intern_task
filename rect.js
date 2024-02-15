f28d43611f1c3fc82d42299a5f70c6c020579593
rathodgautam <104826264+rathodgautam@users.noreply.github.com>
2024-02-15 10:57:30 +0530

    Update rect.js

--- rect.js
+++ rect.js
@@ -1,16 +1,84 @@
-// // const input = [[0,0],[10,0],[0.10],[10,10]]
+import * as THREE from 'three'
+import {x} from './allPoints.js'
+import { ThreeViewer } from './threeCustom/viewer/ThreeViewer'
 
-const a =[10,0,0] , b=[10,10,0] , result=[]
-const n = a.length; 
-
-let mag = 0;
-for(let i=0; i<n; i++){
-    result[i] = a[(i+1)%n]*b[(i+2)%n] - a[(i+2)%n]*b[(i+1)%n];
-    mag +=(result[i]) * (result[i]);
-  }
-  
-  console.log("Final Vector :",result)
-  console.log("Magnitude of Vector:",  Math.sqrt(mag))
+let threeViewer = new ThreeViewer({
+    element: document.getElementById('container3D')
+})
+threeViewer.initViewer()
+threeViewer.loadENV('./env.hdr')
 
 
-  console.log(0-0)
+// Start your project from here
+
+// Create a box
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 })
+// const cube = new THREE.Mesh(geometry, material)
+// threeViewer.scene.add(cube)
+
+threeViewer.intersectionCallback = function checkInterSectionClick() {
+    var raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(this.mouse, this.camera);
+    var intersectionClick = raycaster.intersectObjects(this.pickingObjects, true);
+    if (intersectionClick.length > 0) {
+        console.log(intersectionClick[0])
+    }
+}
+
+
+function addPoint(inLocation,deci) {
+    const sphereGeo = new THREE.SphereGeometry(0.05, 32, 32)
+    
+    const colorOptions = {
+        1: { color: 'red' },  // Color option for deci == 1
+        2: { color: 'white' },  // Color option for deci == 2
+        3: { color: 'black' },  // Color option for deci == 3
+        4: { color: 'orange' } ,  // Color option for deci == 4
+        5: { color: 'brown' }   // Color option for deci == 4
+    };
+    
+    const defaultColor = { color: 'pink' };  // Default color option
+    const sphereMat = new THREE.MeshBasicMaterial(colorOptions[deci] || defaultColor)
+
+
+    const sphere = new THREE.Mesh(sphereGeo, sphereMat)
+    threeViewer.scene.add(sphere)
+    sphere.position.copy(inLocation)
+}
+ 
+for(let i = 0 ; i < x.target.length; i++){ 
+        addPoint(new THREE.Vector3(x.target[i][0],x.target[i][1],x.target[i][2]),x.decision[i])
+    }
+
+
+    addPoint(new THREE.Vector3())
+
+
+function addLine(inP1, inP2) {
+    const material = new THREE.LineBasicMaterial({
+        color: 0x0000ff
+    });
+
+    const points = [];
+    points.push(inP1, inP2)
+
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    const line = new THREE.Line(geometry, material);
+    threeViewer.scene.add(line);
+}
+
+let Plength = x.points.length;
+for(let i = 0 ; i < x.points.length; i++){
+    let a = x.points[i][0], p = x.points[(i+1)%Plength][0];
+    let b = x.points[i][1], q = x.points[(i+1)%Plength][1];
+    let c = x.points[i][2], r = x.points[(i+1)%Plength][2]; 
+    addLine(new THREE.Vector3(a,b,c), new THREE.Vector3(p,q,r))
+}
+
+// addLine(new THREE.Vector3(3.2, 4.2, 0), new THREE.Vector3(10000, 4.2, 0))
+addLine(new THREE.Vector3(3.2, 4.2, 0), new THREE.Vector3(10000, 4.2, 0))
+[ 3.2, 4.2, 0 ] [ 10000, 4.2, 0 ]
+ 
+
